855856463257c781c935035ac6891182168f6803
root <cooperg122@gmail.com>
2025-07-25 13:38:43 -0600

    started with python visual

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 Vehicle Routing Problem
 
 ## Dependencies
-- **SFML** (for visualization)
 - **Catch2** (for testing)
+
 ### WSL Installation
 sudo apt install libsfml-dev
 
-### To do
-Add distance matrix test
---
+++ visuals/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+# Get the customer and depot locations
+def get_locations(file_name):
+    routes_df = pd.read_csv(file_name, header=None)
+
+    location_coords = list(routes_df.iloc[0])[0:-1]
+    locations = []
+    for i in range(0, len(location_coords), 2):
+        locations.append([location_coords[i], location_coords[i+1]])
+
+    return locations
+    
+def get_routes(file_name):
+    routes_df = pd.read_csv(file_name, header=None, skiprows=1)
+
+    routes = []
+    for _, row in routes_df.iterrows():
+        routes.append(list(row.dropna().astype(int))[0:-1])
+    
+    return routes
+
+
+def main():
+    file = "routes.csv"
+    locations = get_locations(file)
+    routes = get_routes(file)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
